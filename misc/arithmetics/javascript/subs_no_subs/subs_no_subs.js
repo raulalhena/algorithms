0c1e25f66c4cff6d68b1586aa644c464ed629c87
Raul <raul.lopezm@gmail.com>
2022-01-05 00:36:54 +0100

    subs_no_subs algorithm js added

---
+++ misc/arithmetics/javascript/subs_no_subs/subs_no_subs.js
@@ -0,0 +1,17 @@
+/**
+ *
+ *  Subtract without subtracting algorithm.
+ *  Notice that there is not number control, so it's asumed that a is going to be
+ *  bigger than b.
+ *
+ */
+
+const subs_no_subs = (a, b) => {
+	let aux = b;
+
+  	for(i = 0; aux < a; i++, aux++);
+
+  console.log(`Result of ${a} - ${b} = ${i}`);
+}
+
+subs_no_subs(10, 3);
